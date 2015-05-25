607e6516d4ce2628beca49c63a3519c29234b063
Sean <sean@tox.im>
2015-05-25 02:30:11 -0700

    flask

---
+++ web.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Py2VM web rewrite WIP, source: https://github.com/stqism/py2vm"
+
+if __name__ == "__main__":
+    app.run()
